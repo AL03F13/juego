66fa61e34c62478aaab454a7fc954e98d6ef80c0
AL03F13 <aponteeric95@gmail.com>
2015-05-19 14:41:59 -0500

    uno
    a jugar

---
+++ js/acciones.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+$(document).ready(function(e) {
+//document.addEventListener("deviceready",function(){
+
+		$('#colormorado').on('click', function(){
+			$('#resultado').append('1,')
+		});
+		$('#colornaranja').on('click', function(){
+			$('#resultado').append('2,')
+		});
+		$('#colorrojo').on('click', function(){
+			$('#resultado').append('3,')
+		});
+		$('#colorazul').on('click', function(){
+			$('#resultado').append('4,')
+		});
+	
+	
+//}); 
+});
+
